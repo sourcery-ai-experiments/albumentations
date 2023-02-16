930406bfc07bb40d059128ff812317bab17d7122
Mikhail Druzhinin <dipet@gmail.com>
2023-02-16 15:45:05 +0700

    WIP: compute profiling by classes

--- albumentations/profiling.py
+++ albumentations/profiling.py
@@ -7,6 +7,7 @@ from typing import Callable, Dict, Optional, Tuple, Type, TypeVar
 
 from typing_extensions import ParamSpec
 
+from albumentations import BaseCompose
 from .utils import BaseAndComposeType, get_transforms_with_compose
 
 __all__ = ["Profiler"]
@@ -17,9 +18,35 @@ T = TypeVar("T")
 
 _PROFILER_RUN_NAME = "Profiling"
 
+_BBOX_METHOD = "apply_to_bbox"
+_BBOXES_METHOD = "apply_to_bboxes"
+_KEYPOINT_METHOD = "apply_to_keypoint"
+_KEYPOINTS_METHOD = "apply_to_keypoints"
+_IMAGE_METHOD = "apply"
+_MASK_METHOD = "apply_to_mask"
+_MASKS_METHOD = "apply_to_masks"
+_ALL_TARGETS_CALL = "__call__"
+_METHODS_TO_PROFILE = (
+    _ALL_TARGETS_CALL,
+    _MASK_METHOD,
+    _BBOX_METHOD,
+    _BBOXES_METHOD,
+    _KEYPOINT_METHOD,
+    _KEYPOINTS_METHOD,
+    _MASK_METHOD,
+    _MASKS_METHOD,
+)
+
 
 class ProfilerData:
-    def __init__(self, name: str, dt: float, profile_data: Optional["ProfilerData"] = None):
+    def __init__(
+        self,
+        cls: Optional[Type[BaseAndComposeType]],
+        name: str,
+        dt: float,
+        profile_data: Optional["ProfilerData"] = None
+    ):
+        self.cls = cls
         self.name = name
         self.dt = dt
         self.profile_data = profile_data
@@ -56,21 +83,32 @@ class ProfilerData:
         self._avg_time = self.dt
         self._max_time = self.dt
 
-        def process(data: Optional[ProfilerData], result_data: dict) -> ProfilerData:
-            data._total_time = data.dt
-            data._min_time = data.dt
-            data._avg_time = data.dt
-            data._max_time = data.dt
+        by_obj = {}
+        by_stack = {}
+        sequence_call = []
 
+        def add_func(data: ProfilerData, result: dict) -> None:
+            if data.cls not in result:
+                result[data.cls] = {}
+            if data.name not in result[data.cls]:
+                result[data.cls][data.name] = []
+            result[data.cls][data.name].append(data)
+
+        def process(data: Optional[ProfilerData], stack_data: dict) -> None:
             if data.profile_data is None:
-                return data
+                return
 
-            if data.name not in result_data:
-                result_data[data.name] = []
-            result_data[data.name].append(data)
-            process(data.profile_data, result_data)
+            add_func(data, by_obj)
+            sequence_call.append(data)
 
-        process(self.profile_data, self._results)
+            cur_stack_data = stack_data
+            if issubclass(data.cls, BaseCompose):
+                cur_stack_data = {}
+
+            add_func(data, stack_data)
+            process(data.profile_data, cur_stack_data)
+
+        process(self.profile_data, by_stack)
 
 
 class Profiler:
@@ -84,7 +122,7 @@ class Profiler:
 
         # Set wrapper
         for name, (cls, func_name, func) in self._original_functions.items():
-            setattr(cls, func_name, self._profile_wrapper(name, func))
+            setattr(cls, func_name, self._profile_wrapper(cls, name, func))
 
     def __del__(self):
         # Remove profile wrapper
@@ -95,25 +133,15 @@ class Profiler:
     def _get_functions() -> Dict[str, Tuple[BaseAndComposeType, str, Callable]]:
         transforms = get_transforms_with_compose()
 
-        wrapped_methods_names = (
-            "__call__",
-            "apply",
-            "apply_to_bbox",
-            "apply_to_bboxes",
-            "apply_to_keypoint",
-            "apply_to_keypoints",
-            "apply_to_mask",
-            "apply_to_masks",
-        )
         res = {}
         for cls_obj in transforms:
             cls_name = cls_obj.get_class_fullname()
-            for name in wrapped_methods_names:
+            for name in _METHODS_TO_PROFILE:
                 if hasattr(cls_obj, name):
                     res[f"{cls_name}.{name}"] = cls_obj, name, getattr(cls_obj, name)
         return res
 
-    def _profile_wrapper(self, name: str, func: Callable[P, T]) -> Callable[P, T]:
+    def _profile_wrapper(self, cls: Type[BaseAndComposeType], name: str, func: Callable[P, T]) -> Callable[P, T]:
         @wraps(func)
         def wrapped_func(*args: P.args, **kwargs: P.kwargs) -> T:
             if not self._started:
@@ -123,7 +151,7 @@ class Profiler:
             res = func(*args, **kwargs)
             dt = time.time() - s
 
-            self._current_run = ProfilerData(name=name, dt=dt, profile_data=self._current_run)
+            self._current_run = ProfilerData(cls=cls, name=name, dt=dt, profile_data=self._current_run)
             return res
 
         return wrapped_func
@@ -142,7 +170,9 @@ class Profiler:
         self._last_run = self._current_run
         self._current_run = None
         self._started = False
-        self._last_run = ProfilerData(_PROFILER_RUN_NAME, time.time() - self._t_start, self._last_run)
+        self._last_run = ProfilerData(
+            cls=None, name=_PROFILER_RUN_NAME, dt=time.time() - self._t_start, profile_data=self._last_run
+        )
 
     def __enter__(self) -> "Profiler":
         self.start()
